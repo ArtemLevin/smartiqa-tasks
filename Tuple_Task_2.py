ad81088d1e576683f86cc7c383252cc580356342
ArtemLevin <python_study_2022@mail.ru>
2023-03-13 21:46:37 +0300

    Task 2

---
+++ Tuple_Task_2.py
@@ -0,0 +1,15 @@
+# Функция slicer() на вход принимает кортеж и случайный элемент. Требуется вернуть новый кортеж, начинающийся
+# с первого появления элемента в нем и заканчивающийся вторым его появлением включительно.
+# Если элемента нет вовсе – вернуть пустой кортеж.
+# Если элемент встречается только один раз, то вернуть кортеж,
+# который начинается с него и идет до конца исходного.
+
+def slicer(my_tuple, element: int):
+    if element not in my_tuple: return ()
+    elif my_tuple.count(element) == 1: return my_tuple
+    else:
+        index = my_tuple.index(element)
+        return tuple(list(my_tuple)[index:(my_tuple.index(element, index + 1) + 1)])
+
+
+print(slicer([10, 2, 3, 4, 10, 6, 7], 1))
