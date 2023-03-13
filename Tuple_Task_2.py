3546f3a1425121c204bd3ab958f687c8e21e14a1
ArtemLevin <python_study_2022@mail.ru>
2023-03-13 21:53:10 +0300

    Task 3

--- Tuple_Task_2.py
+++ Tuple_Task_2.py
@@ -5,11 +5,14 @@
 # который начинается с него и идет до конца исходного.
 
 def slicer(my_tuple, element: int):
-    if element not in my_tuple: return ()
-    elif my_tuple.count(element) == 1: return my_tuple
+    if element not in my_tuple:
+        return ()
+    elif my_tuple.count(element) == 1:
+        return my_tuple
     else:
         index = my_tuple.index(element)
         return tuple(list(my_tuple)[index:(my_tuple.index(element, index + 1) + 1)])
 
 
 print(slicer([10, 2, 3, 4, 10, 6, 7], 1))
+
---
+++ Tuple_Task_3.py
@@ -0,0 +1,13 @@
+# Перед студентом стоит задача: на вход функции sieve() поступает список целых чисел.
+# В результате выполнения этой функции будет получен кортеж уникальных элементов списка в обратном порядке.
+from random import random, randint
+
+numbers = [randint(1, 5) for _ in range(15)]
+print(numbers)
+
+
+def sieve(list_):
+    return tuple(sorted(list(set(list_))))
+
+
+print(sieve(numbers))
